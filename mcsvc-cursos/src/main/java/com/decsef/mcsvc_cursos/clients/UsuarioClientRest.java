2184b46859a77d4808b6907b3e6408648236b2f1
juanengine <jp.rodriguezcam@gmail.com>
2025-01-07 16:08:54 -0600

    docker files

--- mcsvc-cursos/src/main/java/com/decsef/mcsvc_cursos/clients/UsuarioClientRest.java
+++ mcsvc-cursos/src/main/java/com/decsef/mcsvc_cursos/clients/UsuarioClientRest.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "usuarios-mcrsvc", url = "localhost:8001")
+@FeignClient(name = "usuarios-mcrsvc", url = "usuarios-mcrsvc:8001")
 public interface UsuarioClientRest {
 
     @GetMapping("/{id}")
--- usuarios-mcrsvc/src/main/java/com/decsef/usuarios_mcrsvc/client/CursoClienteRest.java
+++ usuarios-mcrsvc/src/main/java/com/decsef/usuarios_mcrsvc/client/CursoClienteRest.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "mcsvc-cursos", url = "localhost:8002")
+@FeignClient(name = "mcsvc-cursos", url = "msvc-cursos:8002")
 public interface CursoClienteRest {
 
     @DeleteMapping("/eliminar-curso-usuario/{id}")
